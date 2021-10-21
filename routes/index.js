506d4dcea9c287ac827c86d24b0362f6c648312f
Adam Cranch <acranch@liquidanimation.com>
2021-10-21 13:28:23 +1000

    initialize again

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+export default function routes(app, addon) {
+    // Redirect root path to /atlassian-connect.json,
+    // which will be served by atlassian-connect-express.
+    app.get('/LiquidBoxConnector', (req, res) => {
+        res.redirect('/atlassian-connect.json');
+    });
+
+    app.get('/LiquidBoxConnector/box', addon.authenticate(), (req, res) => {
+        res.render(
+          'box.jsx', // change this to 'hello-world.jsx' to use the Atlaskit & React version
+          {
+            title: req.query.title,
+            spaceId: req.query.spaceId,
+            spaceKey: req.query.spaceKey
+          }
+        );
+    });
+
+    // Add additional route handlers here...
+}
